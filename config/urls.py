731273dc59a74162d46542a6821b8635213cd00e
morethanmin <mini4614@gmail.com>
2021-08-10 16:53:03 +0900

    for deploy

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from django.conf import settings 
 from django.conf.urls.static import static
+from django.contrib.staticfiles.views import serve
 
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
+    url(r'^static/(?P<path>.*)', serve, kwargs={'insecure': True})
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
